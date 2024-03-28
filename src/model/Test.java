1d023e1dee636146f2fb99f2f745a37e3ab640a6
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2024-03-28 16:40:27 +0530

    Created Test main class

--- src/model/Test.java
+++ src/model/Test.java
@@ -1,9 +1,18 @@
 package model;
 
+import org.apache.log4j.lf5.util.Resource;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
 public class Test {
 
-	public Test() {
-		// TODO Auto-generated constructor stub
-	}
-
+	public static void main(String[] args) {  
+	    ClassPathResource r=new ClassPathResource("applicationContext.xml");  
+	    BeanFactory factory=new XmlBeanFactory(r);  
+	      
+	    Question q=(Question)factory.getBean("q");  
+	    q.displayInfo();  
+	      
+	}  
 }
--- src/model/User.java
+++ src/model/User.java
@@ -2,8 +2,18 @@ package model;
 
 public class User {
 
-	public User() {
-		// TODO Auto-generated constructor stub
-	}
+	private int id;  
+	private String name,email;  
+	public User() {}  
+	public User(int id, String name, String email) {  
+	    super();  
+	    this.id = id;  
+	    this.name = name;  
+	    this.email = email;  
+	}  
+	  
+	public String toString(){  
+	    return "Id:"+id+" Name:"+name+" Email Id:"+email;  
+	}  
 
 }
