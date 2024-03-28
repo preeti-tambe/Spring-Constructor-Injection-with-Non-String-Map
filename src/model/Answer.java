fda6d78cfb67c78b90eeb96756276f218da0a699
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2024-03-28 16:30:54 +0530

    Created Answer class

--- src/model/Answer.java
+++ src/model/Answer.java
@@ -1,9 +1,21 @@
 package model;
 
+import java.util.Date;
+
 public class Answer {
 
-	public Answer() {
-		// TODO Auto-generated constructor stub
-	}
-
+	private int id;  
+	private String answer;  
+	private Date postedDate;  
+	public Answer() {}  
+	public Answer(int id, String answer, Date postedDate) {  
+	    super();  
+	    this.id = id;  
+	    this.answer = answer;  
+	    this.postedDate = postedDate;  
+	}  
+	  
+	public String toString(){  
+	    return "Id:"+id+" Answer:"+answer+" Posted Date:"+postedDate;  
+	}  
 }
